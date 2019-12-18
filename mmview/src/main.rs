ca0f2873899ee19d11642fb2ff74b8dec80a3442
Michael Woerister <michaelwoerister@posteo>
2019-12-18 10:51:59 +0100

    Make output of mmview more readable.

--- mmview/src/main.rs
+++ mmview/src/main.rs
@@ -1,7 +1,7 @@
-use analyzeme::ProfilingData;
+use analyzeme::{Event, ProfilingData, Timestamp};
 use std::error::Error;
 use std::path::PathBuf;
-
+use std::time::{Duration, SystemTime};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -18,6 +18,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let data = ProfilingData::new(&opt.file_prefix)?;
 
+    let global_start_time = data.iter().map(|e| e.timestamp.start()).min().unwrap();
+
     for event in data.iter() {
         if let Some(thread_id) = opt.thread_id {
             if event.thread_id != thread_id {
@@ -25,8 +27,40 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
         }
 
-        println!("{:?}", event);
+        print_event(&event.to_event(), global_start_time);
     }
 
     Ok(())
+}
+
+fn system_time_to_micros_since(t: SystemTime, since: SystemTime) -> u128 {
+    t.duration_since(since)
+        .unwrap_or(Duration::from_nanos(0))
+        .as_micros()
+}
+
+fn print_event(event: &Event<'_>, global_start_time: SystemTime) {
+    let additional_data = event.additional_data.join(",");
+
+    let timestamp = match event.timestamp {
+        Timestamp::Instant(t) => {
+            format!("{} μs", system_time_to_micros_since(t, global_start_time))
+        }
+        Timestamp::Interval { start, end } => format!(
+            "{} μs - {} μs",
+            system_time_to_micros_since(start, global_start_time),
+            system_time_to_micros_since(end, global_start_time)
+        ),
+    };
+
+    println!(
+        r#"{{
+    kind: {},
+    label: {},
+    additional_data: [{}],
+    timestamp: {},
+    thread_id: {},
+}}"#,
+        event.event_kind, event.label, additional_data, timestamp, event.thread_id
+    );
 }
